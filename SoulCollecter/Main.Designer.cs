3dab415aa981b4be1ed9de827b4de1de3f3fb680
SoulCollecter <>
2020-12-09 13:47:06 +0800

    init

---
+++ SoulCollecter/Main.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace SoulCollecter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.panel = new System.Windows.Forms.Panel();
+            this.webControl1 = new EO.WinForm.WebControl();
+            this.webView1 = new EO.WebBrowser.WebView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSignInURL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserAgent = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnPick = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.webControl1);
+            this.panel.Location = new System.Drawing.Point(2, 100);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1211, 557);
+            this.panel.TabIndex = 0;
+            // 
+            // webControl1
+            // 
+            this.webControl1.BackColor = System.Drawing.Color.White;
+            this.webControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webControl1.Location = new System.Drawing.Point(0, 0);
+            this.webControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.webControl1.Name = "webControl1";
+            this.webControl1.Size = new System.Drawing.Size(1211, 557);
+            this.webControl1.TabIndex = 1;
+            this.webControl1.Text = "webControl1";
+            this.webControl1.WebView = this.webView1;
+            // 
+            // webView1
+            // 
+            this.webView1.InputMsgFilter = null;
+            this.webView1.ObjectForScripting = null;
+            this.webView1.Title = null;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sign In URL:";
+            // 
+            // txtSignInURL
+            // 
+            this.txtSignInURL.Location = new System.Drawing.Point(97, 9);
+            this.txtSignInURL.Name = "txtSignInURL";
+            this.txtSignInURL.Size = new System.Drawing.Size(1035, 24);
+            this.txtSignInURL.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "User Agent:";
+            // 
+            // txtUserAgent
+            // 
+            this.txtUserAgent.Location = new System.Drawing.Point(96, 40);
+            this.txtUserAgent.Name = "txtUserAgent";
+            this.txtUserAgent.Size = new System.Drawing.Size(1036, 24);
+            this.txtUserAgent.TabIndex = 3;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(1138, 25);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // btnPick
+            // 
+            this.btnPick.Location = new System.Drawing.Point(1138, 57);
+            this.btnPick.Name = "btnPick";
+            this.btnPick.Size = new System.Drawing.Size(75, 23);
+            this.btnPick.TabIndex = 4;
+            this.btnPick.Text = "Pick";
+            this.btnPick.UseVisualStyleBackColor = true;
+            this.btnPick.Click += new System.EventHandler(this.BtnPick_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Domain:";
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(95, 70);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(1037, 24);
+            this.txtDomain.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1222, 669);
+            this.Controls.Add(this.btnPick);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.txtUserAgent);
+            this.Controls.Add(this.txtSignInURL);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "SoulCollecter";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSignInURL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserAgent;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnPick;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDomain;
+        private EO.WinForm.WebControl webControl1;
+        private EO.WebBrowser.WebView webView1;
+    }
+}
+
---
+++ SoulCollecter/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SoulCollecter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            if (!txtUserAgent.Equals(""))
+            {
+                webView1.CustomUserAgent = txtUserAgent.Text;
+            }
+
+            if (!txtSignInURL.Equals(""))
+            {
+                webView1.LoadUrl(txtSignInURL.Text);
+            }
+
+        }
+        private void Main_Load(object sender, EventArgs e)
+        {
+            string path = "./setting.ini";
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader("./setting.ini"))
+                {
+                    string line;
+                    int i = 0;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        switch (i)
+                        {
+                            case 0: txtSignInURL.Text = line; break;
+                            case 1: txtUserAgent.Text = line; break;
+                            case 2: txtDomain.Text = line; goto done;
+                        }
+                        i++;
+                    }
+                    done: Console.WriteLine("Read Complete");
+                }
+            }
+        }
+
+        private void BtnPick_Click(object sender, EventArgs e)
+        {
+            if (txtDomain.Text.Length > 0)
+            {
+                EO.WebEngine.CookieCollection cc = webView1.Engine.CookieManager.GetCookies(txtDomain.Text);
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < cc.Count; i++)
+                {
+                    stringBuilder.Append(cc[i].Name + "=" + cc[i].Value + ";");
+                }
+                string cookie = stringBuilder.ToString();
+                string userAgent = txtUserAgent.Text;
+                if (cookie.Length > 0 && userAgent.Length > 0)
+                {
+                    Encoding encode = Encoding.ASCII;
+                    byte[] byteData = encode.GetBytes("ck=" + stringBuilder.ToString() + "\r\nua=" + userAgent);
+                    string result = Convert.ToBase64String(byteData, 0, byteData.Length);
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    string fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".soul";
+                    saveFileDialog.FileName = fileName;
+                    saveFileDialog.Filter = "soul file(*.soul)|*.soul";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string fullName = saveFileDialog.FileName;
+                        using (StreamWriter file = new StreamWriter(fullName, false))
+                        {
+                            file.Write(result);
+                        }
+                        MessageBox.Show("Saved");
+                    }
+                }
+            }
+        }
+    }
+}
